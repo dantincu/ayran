d827496888cab4c61d2e8da8941cf575f36ca15c
dantincu <tincuvd@gmail.com>
2026-05-13 19:05:27 +0300

    +

--- JsWs/apps/notes/ayran-notes-tauriapp/desktop/src/App.tsx
+++ JsWs/apps/notes/ayran-notes-tauriapp/desktop/src/App.tsx
@@ -1,6 +1,14 @@
+import React from 'react';
 import AppShell from './components/AppShell';
 
 function App() {
+  React.useEffect(() => {
+    const appLoader = document.getElementById('app_loader');
+    const initialStyles = document.getElementById('initial_style');
+    appLoader?.remove();
+    initialStyles?.remove();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 antialiased">
       <AppShell />
